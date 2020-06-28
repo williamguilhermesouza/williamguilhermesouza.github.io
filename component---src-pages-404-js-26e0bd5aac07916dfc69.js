4d27cb631d83c0825eb0fa07d1534448a6667618
williamguilhermesouza <williamguilhermesouza@gmail.com>
2020-06-27 22:05:43 -0300

    Updates

---
+++ component---src-pages-404-js-26e0bd5aac07916dfc69.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{w2l6:function(e,n,a){"use strict";a.r(n);var t=a("q1tI"),l=a.n(t),r=a("7oih");n.default=function(){return l.a.createElement(r.a,{fullMenu:!0},l.a.createElement("section",{id:"wrapper"},l.a.createElement("header",null,l.a.createElement("div",{className:"inner"},l.a.createElement("h2",null,"Página não encontrada"),l.a.createElement("p",null,"Link inválido")))))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-26e0bd5aac07916dfc69.js.map
--- fontAwesome6script.js
+++
@@ -1,6 +0,0 @@
-let scriptEl = document.createElement("script");
-scriptEl.src = "https://kit.fontawesome.com/de34cd2f4b.js";
-scriptEl.crossOrigin = "anonymous";
-if (typeof document !== 'undefined') {
-    document.head.appendChild(scriptEl);
-}
