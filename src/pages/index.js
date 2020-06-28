186dd2f3912985013fc880486ed74a5ba67f7a5d
williamguilhermesouza <williamguilhermesouza@gmail.com>
2020-06-28 11:03:57 -0300

    fixed icons with helmet

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,6 @@ import React from 'react';
 import Layout from '../components/Layout';
 
 import pic1 from '../assets/images/pic01.jpg';
-import pic2 from '../assets/images/pic02.jpg';
 import pic3 from '../assets/images/pic03.jpg';
 import pic4 from '../assets/images/pic04.jpg';
 import pic5 from '../assets/images/pic05.jpg';
