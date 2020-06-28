47e00e1b64529b7f71823662fb1eeb8f7f76a9d3
williamguilhermesouza <williamguilhermesouza@gmail.com>
2020-06-27 21:44:38 -0300

    changed website template using gatsby

--- fontAwesome6script.js
+++ fontAwesome6script.js
@@ -1,4 +1,6 @@
 let scriptEl = document.createElement("script");
 scriptEl.src = "https://kit.fontawesome.com/de34cd2f4b.js";
 scriptEl.crossOrigin = "anonymous";
-document.head.appendChild(scriptEl);
+if (typeof document !== 'undefined') {
+    document.head.appendChild(scriptEl);
+}
